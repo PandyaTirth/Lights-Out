a890fa5ff67e3275e454a3ab3c3f53534b68713f
Admin <pandyatirth08@gmail.com>
2020-10-07 16:23:30 +0530

    Updates

---
+++ precache-manifest.b82eaa6a722fe11870044f8664360bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad45773080018117c257ebdfe5c9ce45",
+    "url": "/Lights-Out/index.html"
+  },
+  {
+    "revision": "9a88f0ada391cd06353a",
+    "url": "/Lights-Out/static/css/main.293ceda1.chunk.css"
+  },
+  {
+    "revision": "f96afac6c718e2c99701",
+    "url": "/Lights-Out/static/js/2.06b46352.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lights-Out/static/js/2.06b46352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a88f0ada391cd06353a",
+    "url": "/Lights-Out/static/js/main.fb13cece.chunk.js"
+  },
+  {
+    "revision": "b2b1e0818a9cf037746b",
+    "url": "/Lights-Out/static/js/runtime-main.8cc46974.js"
+  }
+]);
